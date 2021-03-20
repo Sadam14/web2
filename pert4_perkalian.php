3ba655a6bc851699171e7e565bb67225614eb31e
Sadam14 <80524845+Sadam14@users.noreply.github.com>
2021-03-20 19:41:55 +0700

    Add files via upload

---
+++ pert4_perkalian.php
@@ -0,0 +1,11 @@
+<? php
+echo  "Tabel Perkalian <br> ============= <br>" ;
+
+untuk ($i=15;$i<=45;$i++) {
+jika ( $i%2==0) melanjutkan ;
+lain {
+  $hasil=12*$i;
+  echo  "12*$i=$hasil<br>" ;
+}
+}
+?>
